5f7c9e6d6ee943d5db550bdb627b163519f04f3e
johnkuefler <john@dev-squared.com>
2023-02-15 18:03:03 -0600

    2 files modified, 2 added

--- index.js
+++ index.js
@@ -4,16 +4,33 @@ const url = require('url');
 const fs = require('fs');
 const id = require('./readidmodule.js');
 const age = require('./agegettermodule.js');
+const uc = require('upper-case');
+const lpad = require('@stdlib/string-left-pad');
+const axios = require('axios');
 
 http
   .createServer(function (req, res) {
+    axios
+      .get('https://jsonplaceholder.typicode.com/todos/')
+      .then(function (response) {
+        console.log(response.data);
+      });
+    res.end();
+
+    // res.writeHead(200, { 'Content-Type': 'text/html' });
+    // res.end(uc.upperCase('hello world!!!!!!!'));
+
+    // res.writeHead(200, { 'Content-Type': 'text/html' });
+    // let str = lpad('Padme', 5, '$');
+    // res.end(str);
+
     // use a module
     //res.writeHead(200, { 'Content-Type': 'text/html' });
     //res.end(id.getId(req));
 
     // another module
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(age.getAge(new Date('December 17, 1995')).toString());
+    // res.writeHead(200, { 'Content-Type': 'text/html' });
+    // res.end(age.getAge(new Date('December 17, 1992')).toString());
 
     // read a file
     // fs.readFile('index.html', function (err, data) {
